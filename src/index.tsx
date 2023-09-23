68862a436c9cd7f1cbdf39cf6bb9f6c923e254f9
Joao Eurico <joao.eurico@eeg.tech>
2023-09-23 07:17:17 -0300

    using turso database

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,71 @@
-import { Elysia, t } from 'elysia';
-import { html } from '@elysiajs/html';
-import { db } from './db';
-import { Todo, todos } from './db/schema';
-import * as elements from 'typed-html';
+import { Elysia, t } from "elysia";
+import { html } from "@elysiajs/html";
+import { db } from "./db";
+import { Todo, todos } from "./db/schema";
+import * as elements from "typed-html";
+import { eq } from "drizzle-orm";
+const app = new Elysia()
+  .use(html())
+  .get("/", ({ html }) =>
+    html(
+      <BaseHtml>
+        <body
+          class="flex w-full h-screen justify-center itens-center"
+          hx-get="/todos"
+          hx-trigger="load"
+          hx-swap="innerHTML"
+        ></body>
+      </BaseHtml>
+    )
+  )
+  .get("/todos", async () => {
+    const data = await db.select().from(todos).all();
+    return <TodoList todos={data} />;
+  })
+  .post(
+    "/todos/toggle/:id",
+    async ({ params }) => {
+      const oldTodo = await db
+        .select()
+        .from(todos)
+        .where(eq(todos.id, params.id))
+        .get();
+      const updatedTodo = await db
+        .update(todos)
+        .set({ completed: !oldTodo?.completed })
+        .where(eq(todos.id, params.id))
+        .returning()
+        .get();
+      return <TodoItem {...updatedTodo} />;
+    },
+    // for validation, you can use the following:
+    {
+      params: t.Object({
+        id: t.Numeric(),
+      }),
+    }
+  )
+  .delete(
+    "/todos/:id",
+    async ({ params }) => {
+      await db.delete(todos).where(eq(todos.id, params.id)).run();
+    },
+    { params: t.Object({ id: t.Numeric() }) }
+  )
+  .post(
+    "/todos",
+    async ({ body }) => {
+      const newTodo = await db.insert(todos).values(body).returning().get();
+
+      return <TodoItem {...newTodo} />;
+    },
+    { body: t.Object({ content: t.String({ minLength: 1 }) }) }
+  )
+  .get("/styles.css", () => Bun.file("./tailwind-gen/styles.css"))
+  .listen(13000);
+console.log(
+  `Elysia is running on http://${app.server?.hostname}:${app.server?.port}`
+);
 
 export const BaseHtml = ({ children }: elements.Children) => `
 <!DOCTYPE html>
@@ -21,20 +84,20 @@ ${children}
 
 export function TodoItem({ content, completed, id }: Todo) {
   return (
-    <div class='flex flex-row space-x-3'>
+    <div class="flex flex-row space-x-3">
       <p>{content}</p>
       <input
-        type='checkbox'
+        type="checkbox"
         checked={completed}
         hx-post={`/todos/toggle/${id}`}
-        hx-target='closest div'
-        hx-swap='outerHTML'
+        hx-target="closest div"
+        hx-swap="outerHTML"
       />
       <button
-        class='text-red-600'
+        class="text-red-600"
         hx-delete={`/todos/${id}`}
-        hx-swap='outerHTML'
-        hx-target='closest div'
+        hx-swap="outerHTML"
+        hx-target="closest div"
       >
         X
       </button>
@@ -45,7 +108,7 @@ export function TodoItem({ content, completed, id }: Todo) {
 export function TodoList({ todos }: { todos: Todo[] }) {
   return (
     <div>
-      {todos.map(todo => (
+      {todos.map((todo) => (
         <TodoItem {...todo} />
       ))}
       <TodoForm />
@@ -53,82 +116,15 @@ export function TodoList({ todos }: { todos: Todo[] }) {
   );
 }
 
-const app = new Elysia()
-  .use(html())
-  .get('/', ({ html }) =>
-    html(
-      <BaseHtml>
-        <body
-          class='flex w-full h-screen justify-center itens-center'
-          hx-get='/todos'
-          hx-trigger='load'
-          hx-swap='innerHTML'
-        ></body>
-      </BaseHtml>
-    )
-  )
-  .post('/clicked', () => (
-    <div class='text-blue-600'>You've clicked indeed, haven't you?</div>
-  ))
-  .get('/todos', () => <TodoList todos={db} />)
-  .delete(
-    '/todos/:id',
-    ({ params }) => {
-      const todo = db.find(todo => todo.id === params.id);
-      if (todo) {
-        db.splice(db.indexOf(todo), 1);
-      }
-    },
-    { params: t.Object({ id: t.Numeric() }) }
-  )
-  .post(
-    '/todos',
-    ({ body }) => {
-      if (body.content.lenght === 0) {
-        throw new Error('Content cannot be empty');
-      }
-      const todo: Todo = {
-        id: db.length + 1,
-        content: body.content,
-        completed: false,
-      };
-      db.push(todo);
-      return <TodoItem {...todo} />;
-    },
-    { body: t.Object({ content: t.String() }) }
-  )
-
-  .post(
-    '/todos/toggle/:id',
-    ({ params }) => {
-      const todo = db.find(todo => todo.id === params.id);
-      if (todo) {
-        todo.completed = !todo.completed;
-        return <TodoItem {...todo} />;
-      }
-    },
-    // for validation, you can use the following:
-    {
-      params: t.Object({
-        id: t.Numeric(),
-      }),
-    }
-  )
-
-  .listen(13000);
-console.log(
-  `Elysia is running on http://${app.server?.hostname}:${app.server?.port}`
-);
-
 export function TodoForm() {
   return (
     <form
-      class='flex flex-row space-x-3'
-      hx-post='/todos'
-      hx-swap='beforebegin'
+      class="flex flex-row space-x-3"
+      hx-post="/todos"
+      hx-swap="beforebegin"
     >
-      <input type='text' name='content' class='border border-black' />
-      <button type='submit'>Add</button>
+      <input type="text" name="content" class="border border-black" />
+      <button type="submit">Add</button>
     </form>
   );
 }
